da77968c3bcbbc54f294f084b7239b469431aa29
ZiaoKolo <koloziao2005@gmail.com>
2025-04-24 23:25:21 +0200

    Update settings.py

--- edulearn_project/settings.py
+++ edulearn_project/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0fsf-by^r+@o^$$=byark+^8%m426kb^+0bln5tkrvywurh&d#'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
