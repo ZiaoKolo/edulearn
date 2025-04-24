e24347f8d373c51eaaaff11d35a0115c5f862a34
ZiaoKolo <koloziao2005@gmail.com>
2025-04-24 23:37:34 +0200

    Update settings.py

--- edulearn_project/settings.py
+++ edulearn_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-0fsf-by^r+@o^$$=byark+^8%m426kb^+0bln5tkrvywurh&d#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [https://ziaokolo.pythonanywhere.com/]
+ALLOWED_HOSTS = ['ziaokolo.pythonanywhere.com', 'localhost', '127.0.0.1']
 
 
 # Application definition
